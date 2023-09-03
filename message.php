126ccda831e86afaafc9d214fd278ab72ea9c39f
magtimmag <139396981+magtimmag@users.noreply.github.com>
2023-09-03 07:02:11 +0300

    Update message.php

--- message.php
+++ message.php
@@ -1,11 +1,2 @@
-$myCurl = curl_init();
-curl_setopt_array($myCurl, array(
-    CURLOPT_URL => 'http://target.site.com/form.php',
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_POST => true,
-    CURLOPT_POSTFIELDS => http_build_query(array(/*здесь массив параметров запроса*/))
-));
-$response = curl_exec($myCurl);
-curl_close($myCurl);
-
-echo "Ответ на Ваш запрос: ".$response;
+<?php
+ var_dump($_GET);
